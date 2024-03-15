0ee95a297de3e26fe7dc1429f044358a866b147e
Tufail Shoukat <154624057+TufailShoukat@users.noreply.github.com>
2024-03-15 19:46:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,298 @@
+//!!!!!!!!!!!!!!!!!!!!!JS IF USE WITH SOME EXAMPLE//!!!!!!!!!!!!!!!!!
+
+//(I)CHECK THE NAMES OF MONTHES
+//   var month = prompt(" Enter the Just  Month Number  And Check  The Name of Month'Year.");
+//   if (month == 1 ) {
+//     document.write("<h1><u>JANUARYY</u> IS (1)FIRST MONTH OF YRAR.</h1>");
+     
+//   }
+//  else if ( month == 2){
+//     document.write("<h1><u>FEBUARYY</u> IS (2)SECOND MONTH OF YRAR.</h1>");
+//  }
+   
+//  else if ( month == 3 )
+//  {
+//     document.write("<h1><u>MARACH</u> IS (3)THIRD MONTH OF YRAR.<H1>");
+//  }
+
+//  else if ( month == 4 )
+//  {
+//     document.write("<h1><u>APRAIL</u> IS (4)FOURTH MONTH OF YRAR.<H1>");
+//  }
+
+
+//  else if ( month == 5 )
+//  {
+//     document.write("<h1><u>MAY </u> IS (5)FIFTH MONTH OF YRAR.</H1>");
+//  }
+
+//  else if ( month == 6 )
+//  {
+//     document.write("<h1><u>JUNE</u> IS (6)SIXTH MONTH OF YRAR.<H1>");
+//  }
+
+//  else if ( month == 7 )
+//  {
+//     document.write("<h1><u>JULY</u> IS (7)SEVENTH MONTH OF YRAR.<H1>");
+//  }
+ 
+//  else if ( month == 8 )
+//  {
+//     document.write("<h1><u>AUGUST</u> IS (8)EIGTH MONTH OF YRAR.<H1>");
+//  }
+
+// else if ( month == 9 )
+//  {
+//     document.write("<h1><u>SETEMBER</u> IS (9)NINITH MONTH OF YRAR<H1>");
+//  }
+
+
+//  else if ( month == 10 )
+//  {
+//     document.write("<h1><u>OCTERBER</u> IS (7)TENTH MONTH OF YRAR<H1>");
+//  }
+
+//  else if ( month == 11 )
+//  {
+//     document.write("<h1><u>NOVEMBER</u> IS (7)ELEVENTH MONTH OF YRAR<H1>")
+//  }
+
+//  else if ( month == 12 )
+//  {
+//     document.write("<h1><u>NOVEMBER</u> IS (LAST )12-TWELEVE MONTH OF YRAR<H1>")
+//  }
+
+
+
+
+
+
+
+//(II)CHECK THE RAMADAN DAYS!
+
+// var day = prompt("Day");
+// if (day > 30) {
+//     document.write("Wrong info")
+    
+// }
+// else if ( day > 20){
+//     document.write("<h1>SECOND ASHRA</H1>")
+// }
+// else if (day > 10){
+//     document.write("<h1>SECOND ASHRA</H1>")
+// }
+// else if (day > 0){
+//     document.write("<h1>FIRST ASHRA</H1>")
+// }
+
+
+
+//QUESTIONS & ANSWER EXAMLE
+// var q1 = prompt(' md hafeez shirt number  ');
+// var  q2 =  prompt(' imad wasim shirt number');
+
+// if ( q1 == '8' &&  q2 == '9'  )
+// {   
+//    alert('congrations you passed')
+// }
+// else {
+//       alert('sorry you fail')
+
+
+
+
+//  LOGIN EXAMPLE 
+
+ //var input1 = prompt("Enter The Email Here!");
+// var input2 = prompt("Enter The Password Here!");
+//  if (  input1 == "tufail" && input2 == "1122") {
+//      document.write("<h1> Login Successfully Here!</h1>")
+    
+//  }
+//        else{
+//       document.write  ("<h1>Sorry! Invalid Email or Password.</h1>")
+//        }
+
+
+///ENTER THE MARKS  &  CHECK THE GRADE!
+
+//    var userinput1 = prompt ("Enter the Marks  Here ")
+//      if( userinput1 >= 900  && userinput1 < 1200 )
+//      {
+//         document.write ("<h1>proud of yourself (A+)Grade</h1> ")
+//      }
+
+//    else if( userinput1 >= 700  && userinput1 <900 )
+
+//     document.write("<h1>(A) grade </h1>")
+
+    
+//    else if( userinput1 >= 500  && userinput1 <700 )
+
+//    document.write(" <h1>(B) grade </h1>")
+
+//    else if( userinput1 >= 400  && userinput1 <500 )
+
+//    document.write(" <h1>(C) grade </h1>")
+
+//    else if( userinput1 >= 300  && userinput1 <400 )
+
+//    document.write(" <h1>(D) grade </h1>")
+
+
+//    else if( userinput1 >= 200 && userinput1 <300 )
+
+//    document.write(" <h1>(E) grade </h1>")
+
+//    else if( userinput1 >= 0  && userinput1 <200 )
+
+//    document.write(" <h1>Sorry you Fail</h1> ")
+
+
+
+// !CHECK THEEVEN/ODD NUMBER CHECKER: 
+
+// let input1 =  prompt("Enter the Number Here CHECK EVEN OR ODD NUMBER!")
+// if ( input1 % 2   ) {
+//     document.write("<h1> The Number is <u> Odd</u>.!      </h1>")
+
+// }
+// else {
+//    document.write ("<h1> The Number is <u> Even</u>.!")
+// }
+
+
+
+//!POSTIVE/NEGAIVE NUMBER CHECKER:
+// var userinput1 = prompt ("Enter the Number here CHECK Positive or Negative Number! ");
+//      if( userinput1 > 0   )
+//      {
+//         document.write ("<h1>The Number is <u> Postive</u>.!</h1> ");
+//       }
+
+//    else {      
+//     document.write("<h1> The Number is <u> Negative</u>.!</h1> </h1>");
+//        }
+//     else if( userinput1 == 0 )
+
+// //  
+
+
+
+// GRADE CALCUATE:
+// var userinput5 = prompt ("Enter the Marks Here English's out of (100) ")
+// var userinput6 =   prompt ("Enter the Marks Here Math's! out of (100) ")
+// var userinput6 =   prompt ("Enter the Marks Here Sceince's!  out of (100)")
+// var userinput7 =   prompt ("Enter the here total Passed Marks Out of (300)")
+
+
+//      if( userinput5 >= 80  && userinput5 < 100 )
+//      {
+//         document.write ("<h1>Proud of yourself (A+)Grade</h1> ")
+
+//      } 
+
+//      else if( userinput5 >= 70 && userinput5 <80 )
+
+//          document.write("<h1>(A) grade </h1>")
+
+//          else if( userinput5 >= 60 && userinput5 <70 )
+
+//          document.write("<h1>(B) grade </h1>")
+
+//          else if( userinput5 >= 50 && userinput5 <60 )
+
+//          document.write("<h1>(C)  Grade </h1>")
+
+
+//          else if( userinput5 >=  40 && userinput5 <50 )
+
+//          document.write("<h1>(D)  Grade </h1>")
+//          else if( userinput5 >= 30 && userinput5 <40 )
+
+//          document.write("<h1>(E) grade </h1>")
+
+//          else if( userinput5 >= 0 && userinput5 <30 )
+
+//          document.write("<h1> SORRY! You failed try again. </h1>")
+
+
+
+//CHECK THE AGE!
+//  var age = prompt("Enter the Age!")
+//  if (age >= 0  && age < 13) {
+//     document.write("<H1>(CHILD!)</H1>")
+
+//  }
+
+//  else if( age  >=14 && age<19 )
+
+//            document.write("<h1> (Teenger!) </h1>")
+
+
+//  else if( age => 20 && age<65 )
+
+//  document.write("<h1> (Adult!) </h1>")
+
+//  else if( age >= 65 && age<1000 )
+
+//  document.write("<h1> (Senior!) </h1>")
+
+
+//CHECK THE GRADE USE THE BORAD MARKS!
+// var marks = prompt("Enter the Marks here Out of (1200) & Check the(Grade) online!");
+// if (marks > 999) {
+//    document.write("<h1> EXCLLENT! Proud of yourself Your Grade is <u> A+</u></h1> ")
+
+// }
+
+// else if (marks > 799) {
+//    document.write("<H1>GOOD! Your Grade is <u>A</u> </h1> ");
+// }
+
+
+// else if (marks > 599) {
+//    document.write("<H1> Your Grade is <u>B</u> </h1> ");
+// }
+
+// else if (marks > 499) {
+//    document.write("<H1> Your Grade is <u>C</u> </h1> ");
+// }
+
+// else if (marks > 399) {
+//    document.write("<H1> Your Grade is <u>D</u> </h1> ");
+// }
+
+
+// else if (marks > 299) {
+//    document.write("<H1> Your Grade is <u>E</u> </h1> ");
+// }
+
+
+// else if (marks < 299) {
+//    document.write("<H1> SORRY! YOU ARE  <u>FAILED   </u> Best of luck for Next Time.</h1> ");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
